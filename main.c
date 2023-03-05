f1038b9aff193ec2dd782c4286a13c44de28c185
Daniel Ternovskiy <danielternovski@gmail.com.noreply.github.com>
2023-03-06 00:53:50 +0700

    Add func for check word circle

--- main.c
+++ main.c
@@ -4,10 +4,27 @@
 
 #define SIZE 100
 
-void strtolower(char* str)
+void strToLower(char* str)
 {
     for (int i = 0; i < strlen(str); i++)
         str[i] = tolower(str[i]);
+}
+
+int checkFigure(char* str)
+{
+    int ret = 1;
+    char rec[SIZE];
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] != '(')
+            rec[i] = str[i];
+        else
+            break;
+    }
+    char figure[] = "circle";
+    if (strcmp(rec, figure) == 0) {
+        ret = 0;
+    }
+    return ret;
 }
 
 int main()
@@ -18,7 +35,7 @@ int main()
     int countFigures = 0;
     while (fgets(str1, SIZE, file)) {
         countFigures++;
-        strtolower(str1);
+        strToLower(str1);
         printErrors(str1, countFigures);
     }
     fclose(file);
