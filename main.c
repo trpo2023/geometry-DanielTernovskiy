9d33c479cc179a54f33abc2639b75b2679817a65
Daniel Ternovskiy <danielternovski@gmail.com.noreply.github.com>
2023-03-06 00:55:27 +0700

    Add func for check arguments

--- main.c
+++ main.c
@@ -27,6 +27,53 @@ int checkFigure(char* str)
     return ret;
 }
 
+int checkArguments(char* str) {
+    int ret = 0;
+    int count = 0;
+    for (int i = 7; str[i] != ',' && i < strlen(str); i++) {
+        if ((str[i] != '.' && str[i] != ' ')
+            && !(str[i] >= 48 && str[i] <= 57)) {
+            printf("Object coordinates entered incorrectly\n\n");
+            ret++;
+            return 1;
+        }
+        if (str[i] >= 48 && str[i] <= 57 && str[i + 1] == ' ')
+            count++;
+        if (str[i] == '.' && str[i + 1] == ')')
+            count += 2;
+    }
+    if (count + 1 != 2) {
+        printf("Object coordinates entered incorrectly\n\n");
+        ret++;
+        return ret;
+    }
+    int index = 0;
+    for (int i = 0; i != strlen(str); i++) {
+        if (str[i] == ',') {
+            index = i + 1;
+            i = strlen(str) - 1;
+        }
+    }
+    for (; str[index] != ')' && index < strlen(str); index++) {
+        if ((str[index] != '.' && str[index] != ' ')
+            && !(str[index] >= 48 && str[index] <= 57)) {
+            printf("Object radius entered incorrectly\n\n");
+            ret++;
+            return 1;
+        }
+        if (str[index] >= 48 && str[index] <= 57 && str[index + 1] == ' ')
+            count++;
+        if (str[index] == '.' && str[index + 1] == ' ')
+            count += 2;
+    }
+    if (count != 1) {
+        printf("Object radius entered incorrectly\n\n");
+        ret++;
+    }
+    return ret;
+}
+
+
 int main()
 {
     FILE* file;
