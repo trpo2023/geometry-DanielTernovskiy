3f83a95050727aedf0cbab66dd3727036e9a43a3
DanielTernovskiy <danielternovski@gmail.com>
2023-04-17 23:30:17 +0700

    Fix code style

--- src/libgeometry/area.c
+++ src/libgeometry/area.c
@@ -7,8 +7,7 @@ float calculateArea(char* str)
 {
     size_t i = 0;
     char* temp = (char*)malloc(sizeof(char));
-    for (i = 0; str[i] != ','; i++)
-        ;
+    for (i = 0; str[i] != ','; i++);
     i++;
     int index = 0;
     for (; i < strlen(str); i++) {
--- src/libgeometry/perimetr.c
+++ src/libgeometry/perimetr.c
@@ -8,8 +8,7 @@ float calculatePerimetr(char* str)
 {
     size_t i = 0;
     char* temp = (char*)malloc(sizeof(char));
-    for (i = 0; str[i] != ','; i++)
-        ;
+    for (i = 0; str[i] != ','; i++);
     i++;
     int index = 0;
     for (; i < strlen(str); i++) {
