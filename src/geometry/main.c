2f3706fdedef17a69168da855f7411ca12a0cf75
DanielTernovskiy <danielternovski@gmail.com>
2023-04-17 14:31:23 +0700

    New main file

---
+++ src/geometry/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 100
+
+int main()
+{
+    FILE* file;
+    file = fopen("input.txt", "r");
+    if (file == NULL) {
+        printf("Error of oppening file!");
+        return 1;
+    }
+    char str1[SIZE];
+    int countFigures = 0;
+    while (fgets(str1, SIZE, file)) {
+        countFigures++;
+        strToLower(str1);
+        printErrors(str1, countFigures);
+    }
+    fclose(file);
+    printf("\n");
+    return 0;
+}
