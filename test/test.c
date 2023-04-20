ba3cc1899948edd0b3e45fd004dcae4fa454fc57
DanielTernovskiy <danielternovski@gmail.com>
2023-04-21 01:47:22 +0700

    Add tests

--- test/test.c
+++ test/test.c
@@ -1,7 +1,6 @@
 #include <ctest.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
-#include <string.h>
 
 #include <libgeometry/area.h>
 #include <libgeometry/lexer.h>
@@ -89,4 +88,38 @@ CTEST (check_incorrect_end_bracket, test10)
     const int expected = 1;
     const int result = checkEnd(str);
     ASSERT_EQUAL(expected, result);
+}
+
+CTEST (check_right_area, test11) 
+{
+    char* str = "circle(2 2, 3)";
+    const double expected = 9 * M_PI;
+    const double result = calculateArea(str);
+    ASSERT_DBL_NEAR_TOL(expected, result, 0.00000000001);
+}
+
+CTEST (check_incorrect_area, test12)
+{
+    char* str = "circle(2 2, x)";
+    const double expected = -1;
+    const double result = calculateArea(str);
+    ASSERT_DBL_NEAR_TOL(expected, result, 0);
+
+}
+
+CTEST (check_right_perimetr, test13) 
+{
+    char* str = "circle(2 2, 3)";
+    const double expected = 2 * M_PI * 3;
+    const double result = calculatePerimetr(str);
+    ASSERT_DBL_NEAR_TOL(expected, result, 0.00000000001);
+}
+
+CTEST (check_incorrect_perimetr, test14)
+{
+    char* str = "circle(2 2, x)";
+    const double expected = -1;
+    const double result = calculatePerimetr(str);
+    ASSERT_DBL_NEAR_TOL(expected, result, 0);
+
 }
