0fffdcbf6c50e3ffe7a8caf350cfc147d144f977
DanielTernovskiy <danielternovski@gmail.com>
2023-04-22 15:51:52 +0700

    Edit test

--- test/test.c
+++ test/test.c
@@ -50,7 +50,7 @@ CTEST(check_diff_arg, test5)
 CTEST(check_incorrect_arg, test6)
 {
     char* str = "circle(x 2.0, 3.0)";
-    const int expected = 1;
+    const int expected = 2;
     const int result = checkArguments(str);
     ASSERT_EQUAL(expected, result);
 }
@@ -58,22 +58,22 @@ CTEST(check_incorrect_arg, test6)
 CTEST(check_incorrect_arg, test7)
 {
     char* str = "circle(2.0 x, 3.0)";
-    const int expected = 1;
+    const int expected = 2;
     const int result = checkArguments(str);
     ASSERT_EQUAL(expected, result);
 }
 
 CTEST(check_incorrect_arg, test8)
 {
-    char* str = "circle(2.0 2.0, x)";
-    const int expected = 2;
+    char* str = "circle(2.0 3.0, X)";
+    const int expected = 3;
     const int result = checkArguments(str);
     ASSERT_EQUAL(expected, result);
 }
 
 CTEST(check_end_bracket, test9)
 {
-    char* str = "circle(2.0 2.0, 3.0)";
+    char* str = "circle(1.0 2.0, 3.0)\n ";
     const int expected = 0;
     const int result = checkEnd(str);
     ASSERT_EQUAL(expected, result);
@@ -82,7 +82,7 @@ CTEST(check_end_bracket, test9)
 CTEST(check_incorrect_end_bracket, test10)
 {
     char* str = "circle(2.0 2.0, 3.0(";
-    const int expected = 1;
+    const int expected = 4;
     const int result = checkEnd(str);
     ASSERT_EQUAL(expected, result);
 }
