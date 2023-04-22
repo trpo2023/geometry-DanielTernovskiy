480fb3df29b18e69315335461b0fd951b6894790
DanielTernovskiy <danielternovski@gmail.com>
2023-04-22 16:01:05 +0700

    Format in test.c

--- test/test.c
+++ test/test.c
@@ -87,7 +87,7 @@ CTEST(check_incorrect_end_bracket, test10)
     ASSERT_EQUAL(expected, result);
 }
 
-CTEST (check_right_area, test11) 
+CTEST(check_right_area, test11)
 {
     char* str = "circle(2 2, 3)";
     const double expected = 9 * M_PI;
@@ -95,16 +95,15 @@ CTEST (check_right_area, test11)
     ASSERT_DBL_NEAR_TOL(expected, result, 0.00000000001);
 }
 
-CTEST (check_incorrect_area, test12)
+CTEST(check_incorrect_area, test12)
 {
     char* str = "circle(2 2, x)";
     const double expected = -1;
     const double result = calculateArea(str);
     ASSERT_DBL_NEAR_TOL(expected, result, 0);
-
 }
 
-CTEST (check_right_perimetr, test13) 
+CTEST(check_right_perimetr, test13)
 {
     char* str = "circle(2 2, 3)";
     const double expected = 2 * M_PI * 3;
@@ -112,11 +111,10 @@ CTEST (check_right_perimetr, test13)
     ASSERT_DBL_NEAR_TOL(expected, result, 0.00000000001);
 }
 
-CTEST (check_incorrect_perimetr, test14)
+CTEST(check_incorrect_perimetr, test14)
 {
     char* str = "circle(2 2, x)";
     const double expected = -1;
     const double result = calculatePerimetr(str);
     ASSERT_DBL_NEAR_TOL(expected, result, 0);
-
 }
